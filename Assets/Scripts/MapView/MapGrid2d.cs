87ef3bc94ec4c73ff04c84fa536eb081034e8001
Jack Jarvis <jackpjarvis12@gmail.com>
2023-01-04 22:41:37 +0000

    Fix 2D scene after abstract mapgrid

--- Assets/Scripts/MapView/MapGrid2d.cs
+++ Assets/Scripts/MapView/MapGrid2d.cs
@@ -6,7 +6,7 @@ namespace FireEmblem.MapView
 {
     [RequireComponent(typeof(Tilemap))]
     [RequireComponent(typeof(Grid))]
-    public class MapGrid2d : MonoBehaviour
+    public class MapGrid2d : MapGrid
     {
         private Tilemap _tilemap;
         private Grid _grid;
@@ -17,18 +17,18 @@ namespace FireEmblem.MapView
             _grid = GetComponent<Grid>();
         }
 
-        public IMapTile GetTileAt(MapPosition mapPosition)
+        public override IMapTile GetTileAt(MapPosition mapPosition)
         {
             return _tilemap.GetTile<MapTile>(new Vector3Int(mapPosition.X, mapPosition.Y, 0));
         }
 
-        public void MoveObjectToGridPosition(GameObject objectToMove, MapPosition position)
+        public override void MoveObjectToGridPosition(GameObject objectToMove, MapPosition position)
         {
             objectToMove.transform.position =
                 _grid.GetCellCenterLocal(new Vector3Int(position.X, position.Y, 0));
         }
         
-        public GameObject InstantiateAtGridPosition(GameObject prefab, MapPosition position)
+        public override GameObject InstantiateAtGridPosition(GameObject prefab, MapPosition position)
         {
             var instantiatedObject = Instantiate(prefab, transform);
             MoveObjectToGridPosition(instantiatedObject, position);
