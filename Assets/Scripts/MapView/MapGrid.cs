69f023ddf6abbe93c3970e290e56d99e2ffb4cc8
Jack Jarvis <jackpjarvis12@gmail.com>
2023-01-05 00:25:56 +0000

    Strip out abstract MapGrid

--- Assets/Scripts/MapView/MapGrid.cs
+++ Assets/Scripts/MapView/MapGrid.cs
@@ -1,14 +1,38 @@
 using FireEmblem.Domain.Data;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace FireEmblem.MapView
 {
-    public abstract class MapGrid : MonoBehaviour
+    [RequireComponent(typeof(Tilemap))]
+    [RequireComponent(typeof(Grid))]
+    public class MapGrid : MonoBehaviour
     {
-        public abstract IMapTile GetTileAt(MapPosition mapPosition);
+        private Tilemap _tilemap;
+        private Grid _grid;
+        
+        private void Awake()
+        {
+            _tilemap = GetComponent<Tilemap>();
+            _grid = GetComponent<Grid>();
+        }
 
-        public abstract void MoveObjectToGridPosition(GameObject objectToMove, MapPosition position);
+        public IMapTile GetTileAt(MapPosition mapPosition)
+        {
+            return _tilemap.GetTile<MapTile>(new Vector3Int(mapPosition.X, mapPosition.Y, 0));
+        }
 
-        public abstract GameObject InstantiateAtGridPosition(GameObject prefab, MapPosition position);
+        public void MoveObjectToGridPosition(GameObject objectToMove, MapPosition position)
+        {
+            objectToMove.transform.position =
+                _grid.GetCellCenterLocal(new Vector3Int(position.X, position.Y, 0));
+        }
+        
+        public GameObject InstantiateAtGridPosition(GameObject prefab, MapPosition position)
+        {
+            var instantiatedObject = Instantiate(prefab, transform);
+            MoveObjectToGridPosition(instantiatedObject, position);
+            return instantiatedObject;
+        }
     }
 }
--- Assets/Scripts/MapView/MapGrid2d.cs
+++ Assets/Scripts/MapView/MapGrid2d.cs
@@ -4,35 +4,8 @@ using UnityEngine.Tilemaps;
 
 namespace FireEmblem.MapView
 {
-    [RequireComponent(typeof(Tilemap))]
-    [RequireComponent(typeof(Grid))]
     public class MapGrid2d : MapGrid
     {
-        private Tilemap _tilemap;
-        private Grid _grid;
         
-        private void Awake()
-        {
-            _tilemap = GetComponent<Tilemap>();
-            _grid = GetComponent<Grid>();
-        }
-
-        public override IMapTile GetTileAt(MapPosition mapPosition)
-        {
-            return _tilemap.GetTile<MapTile>(new Vector3Int(mapPosition.X, mapPosition.Y, 0));
-        }
-
-        public override void MoveObjectToGridPosition(GameObject objectToMove, MapPosition position)
-        {
-            objectToMove.transform.position =
-                _grid.GetCellCenterLocal(new Vector3Int(position.X, position.Y, 0));
-        }
-        
-        public override GameObject InstantiateAtGridPosition(GameObject prefab, MapPosition position)
-        {
-            var instantiatedObject = Instantiate(prefab, transform);
-            MoveObjectToGridPosition(instantiatedObject, position);
-            return instantiatedObject;
-        }
     }
 }
